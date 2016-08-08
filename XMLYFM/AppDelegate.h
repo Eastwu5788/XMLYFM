e29cb456cd6183de2b82955560f7c0d192adfa26
PPTT <pptt@ppttdembp.lan>
2016-08-08 11:39:44 +0800

    Initial Commit

---
+++ XMLYFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMLYFM
+//
+//  Created by East_wu on 16/8/8.
+//  Copyright © 2016年 East_wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMLYFM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XMLYFM
+//
+//  Created by East_wu on 16/8/8.
+//  Copyright © 2016年 East_wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
